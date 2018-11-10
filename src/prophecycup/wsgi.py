e02437e7bf926d60ead85ac0d75210e09212a9dd
ew4321 <andrewykim99@gmail.com>
2018-11-09 21:39:02 -0500

    started prophecycup project. added blog app.

---
+++ src/prophecycup/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for prophecycup project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prophecycup.settings")
+
+application = get_wsgi_application()
