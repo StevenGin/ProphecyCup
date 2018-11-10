768d58f36fd7cf984ccff005d73cde8dd357f0e8
ew4321 <andrewykim99@gmail.com>
2018-11-09 22:15:16 -0500

    changed prophecycup/urls.py to include urls from articles app

--- src/articles/urls.py
+++ src/articles/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
-from django.contrib import admin
-
-from blog.views import home
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
+    url(r'^$', views.article_list),
 ]
