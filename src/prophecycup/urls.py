cc42514a5d93065dd71a500c2aee9675da73f49d
ew4321 <andrewykim99@gmail.com>
2018-11-09 22:02:55 -0500

    created separate urls.py and views.py for prophecycup (project) and articles (app)."
    "

--- src/prophecycup/urls.py
+++ src/prophecycup/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from blog.views import home
+from . import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
+    url(r'^about/$', views.about),
+    url(r'^$', views.home),
 ]
